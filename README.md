88aaf871329b343c8c6e8cb0d976fb6efbd95069
Matthew Festa <festa.matthew@me.com>
2022-02-25 21:18:04 -0500

    added a round function that usese the parameters of computerSelection and playerSelection to calculate each play

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rock-paper-scissors
+
 Simple JavaScript practice of rock paper scissors
 first iteration will be that it is playable directly from the console. 
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //this array will be universally declared
 const choices = ["rock", "paper", "scissors"];
+// created a variable computerSelection to store for later
+const computerSelection = computerPlay();
+// both computer and player scores will start at zero. 
+let computerScore = 0;
+let playerScore = 0;
 
 //created this function to allow the computer to randomly generated one of the choices in the array
 function computerPlay(arr) {
@@ -10,6 +15,43 @@ function computerPlay(arr) {
     // we want to make sure that we return either rock, paper or scissors each play. 
 	return item;
 }
+//accounts for the actual gameplay using boolean values and incrementation of score
+function playRound(playerSelection, computerSelection) {
 
-// created a variable computerResult to store for later
-const computerResult = computerPlay(choices);
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore += 1;
+        return computerSelection + ' beats ' + playerSelection + '. Try Again!';
+
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScoreScore += 1;
+        return playerSelection + ' beats ' + computerSelection + '. Great job!';
+
+    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
+        return 'Draw!';	
+
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore += 1;
+        return playerSelection + ' beats ' + computerSelection + '. Great job!';
+
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore += 1;
+       return computerSelection + ' beats ' + playerSelection + '. Try Again!';
+
+    } else if (playerSelection == 'paper' + computerSelection == 'paper') {
+        return 'Draw!';	
+    
+    } else if (playerSelection == 'scissors' + computerSelection == 'rock') {
+        computerScore +=1;
+        return computerSelection + ' beats ' + playerSelection + '. Try Again!';
+    
+    } else if (playerSelection == 'scissors' + computerSelection == 'paper') {
+        playerScore += 1;
+        return playerSelection + ' beats ' + computerSelection + '. Great job!';
+    
+    } else if (playerSelection == 'scissors' + computerSelection == 'scissors') {
+        return 'Draw!';	
+    
+    } else {
+        return 'Invalid Round. Try again. '
+    }    
+}   
